eabec75386edbb96e041f1ee14d44af5c450dead
basharinandrej <basharin_andrej@mail.ru>
2020-10-09 13:25:04 +0300

    Доделал адаптив

--- src/index.js
+++ src/index.js
@@ -8,5 +8,75 @@ import "slick-carousel"
 
 $('.main-slider').slick({
     arrows: false,
-    dots: true
+    dots: true,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    responsive: [
+        {
+          breakpoint: 1151,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: false
+          }
+        },
+        {
+          breakpoint: 993,
+          settings: {
+            variableWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            variableWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    ]
 });
+
+
+$('.burger--js').click( function(e) {
+    e.preventDefault();
+    $('.header__main-btn').addClass('translateX-50').css('left', '50%').css('bottom', '20px').css('top', 'auto');
+    $('.header__main-menu').addClass('translateX');
+    $(this).hide();
+    $('.close-icon--js').show();
+    $('body').addClass('hidden')
+})
+
+$('.close-icon--js').click( function() {
+  $(this).hide();
+  $('.burger--js').show();
+  $('.header__main-btn').removeClass('translateX-50').css('left', '0%');
+  $('.header__main-menu').removeClass('translateX');
+  $('body').removeClass('hidden')
+})
+
+
+
+
+/* Дропдаун высоты */
+var acc = document.getElementsByClassName('dropdown-btn');
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function (e) {
+        e.preventDefault();
+        this.classList.toggle("active");
+        var id = $(this).attr('data-dropdown');
+
+        var panel = document.querySelector('#' + id);
+        panel.classList.toggle("active");
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
+/* Дропдаун высоты конец*/
